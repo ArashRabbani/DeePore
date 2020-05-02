4c3ddcc552b02632d760ba66cf7723a129842026
ArashRabbani <rabarash@gmail.com>
2020-05-02 20:51:33 +0100

    Some Changes

--- DeePore.py
+++ DeePore.py
@@ -157,3 +157,23 @@ def testmodel(model,DataName,TestList,MIN,MAX):
     y2=np.multiply(y2,(MAX-MIN))+MIN
     y[:,0]=10**y[:,0]
     y2[:,0]=10**y2[:,0]
+    # #  Show prediction of 15 single-value features
+    import pandas as pd
+    fig=plt.figure(figsize=(30,40))
+    plt.rcParams.update({'font.size': 30})
+    df = pd.read_excel('VarNames.xlsx')
+    for I in range(15):
+        ax = fig.add_subplot(5,3,I+1)
+        X=y[:,I]
+        Y=y2[:,I]
+        plt.scatter(X,Y)
+        plt.ylabel('Predicted')
+        plt.xlabel('Ground truth')
+        plt.tick_params(direction="in")
+        plt.text(.5,.9,df.Value[I],horizontalalignment='center',transform=ax.transAxes)
+        plt.xlim(np.min(X),np.max(X))
+        plt.ylim(np.min(Y),np.max(Y))
+        if I==0:
+            ax.set_yscale('log')
+            ax.set_xscale('log')
+    plt.savefig('Single-value_Features.png')
